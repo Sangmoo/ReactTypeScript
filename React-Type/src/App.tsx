36a68d05308b8a02ba35291f8d21f7f9ffa43cee
Sangmoo <first_1st@naver.com>
2020-03-05 01:06:36 +0900

    Use Counter Component

--- React-Type/src/App.tsx
+++ React-Type/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Profile from './01/PropsComponent';
-import ChildComponent from './01/ChildComponent';
+import Counter from './01/Counter';
 
 class App extends React.Component {
   render() {
@@ -9,9 +9,9 @@ class App extends React.Component {
     const obj = { name: '상무', age: 28 };
     return (
       <div>
-        <Profile name="상무" job="프로그래머" />{' '}
         {/* name과 job 인터페이스를 사용하기때문에, name과 job에 데이터 입력해야 실행 가능 */}
-        <ChildComponent boolValue={true} numValue={7} arrayValue={array} objValue={obj} />
+        <Profile name="상무" job="프로그래머" />
+        <Counter />
       </div>
     );
   }
