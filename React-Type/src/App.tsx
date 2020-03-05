b6e9a67c66472aeb7ea1fbec4761b32cd6dbd52f
Sangmoo <first_1st@naver.com>
2020-03-06 00:48:24 +0900

    Use Tsx ToDoList

--- React-Type/src/App.tsx
+++ React-Type/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Profile from './01/PropsComponent';
 import Counter from './01/Counter';
+import TodoList from './01/TodoList';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         {/* name과 job 인터페이스를 사용하기때문에, name과 job에 데이터 입력해야 실행 가능 */}
         <Profile name="상무" job="프로그래머" />
         <Counter />
+        <TodoList />
       </div>
     );
   }
